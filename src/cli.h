334cb0ae484ab553fc1d4152235d5c53e314d7a9
Peter Heywood <p.heywood@sheffield.ac.uk>
2026-05-20 17:07:08 +0100

    CUDA 12: Do not include cli11 in .cu files, due to a cudafe memory leak

---
+++ src/cli.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <inttypes.h>
+#include <cfloat>
+#include <filesystem>
+
+// CLI related code
+// CLI11 causes a memory leak in cudafe for CUDA 12.x, so is only used in .cpp file(s)
+
+/**
+ * Struct containing values which can be configured using the CLI
+ */
+struct Arguments {
+    // The GPU index to use
+    std::int32_t device = 0;
+    // The number of times each simulation is repeated
+    std::uint32_t repetitions = 3u;
+    // The number of steps for each simulation
+    std::uint32_t steps = 1000u;
+    // PRNG seed
+    std::uint64_t seed = 0u;
+    // If validation should be performed for this run?
+    bool validation = false;
+    // If a dry run should be performed
+    bool dry_run = false;
+    // The output path for performance data
+    std::filesystem::path output_path = std::filesystem::current_path() / "benchmark-flamegpu.json";
+};
+
+/**
+ * Define and parse the command line interface
+ */
+Arguments parse_cli(int argc, const char ** argv);
